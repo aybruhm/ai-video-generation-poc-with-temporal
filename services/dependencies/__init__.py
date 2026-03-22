de9beaae168b4b0bc348995f96a21f8945aec886
Abram <git.skimming549@passinbox.com>
2026-03-22 13:18:24 +0000

    chore: add temporal_client dependency

--- services/dependencies/__init__.py
+++ services/dependencies/__init__.py
@@ -1,4 +1,6 @@
 from .auth import AuthDependencies
+from .temporal import get_temporal_client
 
 auth_deps = AuthDependencies()
 get_current_user = auth_deps.get_current_user
+get_temporal_client = get_temporal_client
