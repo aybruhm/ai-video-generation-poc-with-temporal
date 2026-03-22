b5b0e1e1ed395306e8287670a10a0bac13a15875
Abram <git.skimming549@passinbox.com>
2026-03-22 14:24:07 +0000

    feat: add users auth routes and DTOs

--- apis/fastapi/routes/dtos.py
+++ apis/fastapi/routes/dtos.py
@@ -1,8 +1,12 @@
+import uuid
 from datetime import datetime
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 from core.generations.dtos import StartGenerationRequestDTO
+from core.users.dtos import LoginUserDTO, RegisterUserDTO
+
+# ================= DTOs for the video generation API
 
 
 class GenerateVideoRequestDTO(StartGenerationRequestDTO):
@@ -19,3 +23,39 @@ class GenerationStatusResponseDTO(BaseModel):
     generation_id: str
     status: str | None
     start_time: datetime
+
+
+# ================= DTOs for the auth API
+
+
+class RegisterUserRequestDTO(RegisterUserDTO):
+    pass
+
+
+class LoginUserRequestDTO(LoginUserDTO):
+    pass
+
+
+class JWTTokensDTO(BaseModel):
+    access_token: str
+    refresh_token: str
+
+
+class CredentialsDTO(JWTTokensDTO):
+    pass
+
+
+class RefreshTokensRequestDTO(BaseModel):
+    refresh_token: str
+
+
+class UserResponse(BaseModel):
+    id: uuid.UUID
+    username: str
+    token_balance: int
+    created_at: datetime
+
+
+class UserWithCredentialsResponse(BaseModel):
+    user: UserResponse
+    credentials: CredentialsDTO
