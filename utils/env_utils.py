ea02fae3e8208c6857556f76105011c9ae5e0677
Abram <git.skimming549@passinbox.com>
2026-03-22 13:18:42 +0000

    chore: add jwt settings to env settings

--- utils/env_utils.py
+++ utils/env_utils.py
@@ -7,15 +7,16 @@ from pydantic import BaseModel
 
 class EnvSettings(BaseModel):
     # Infrastructure Settings
-    TEMPORAL_TARGET_HOST: str = os.getenv(
-        "TEMPORAL_TARGET_HOST", "localhost:7233"
-    )
+    TEMPORAL_TARGET_HOST: str = os.getenv("TEMPORAL_TARGET_HOST", "temporal:7233")
     DATABASE_URL: str = os.getenv("DATABASE_URL", "sqlite+aiosqlite:////tmp/poc.db")
 
     # Application Settings
     TEMPORAL_WORKER_TASK_QUEUE: Dict[str, str] = json.loads(
         os.getenv("TEMPORAL_WORKER_TASK_QUEUE", "{}")
     )
+    JWT_KEY: str = os.getenv("JWT_KEY", "")
+    JWT_EXP: int = int(os.getenv("JWT_EXP", "900"))
+    JWT_ALGORITHM: str = os.getenv("JWT_ALGORITHM", "HS256")
 
 
 env = EnvSettings()
