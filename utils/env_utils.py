706dd412a762b6ef039023329a6ed582bc197a8b
Abram <git.skimming549@passinbox.com>
2026-03-22 12:00:40 +0000

    feat: add env configuration

---
+++ utils/env_utils.py
@@ -0,0 +1,21 @@
+import json
+import os
+from typing import Dict
+
+from pydantic import BaseModel
+
+
+class EnvSettings(BaseModel):
+    # Infrastructure Settings
+    TEMPORAL_TARGET_HOST: str = os.getenv(
+        "TEMPORAL_TARGET_HOST", "localhost:7233"
+    )
+    DATABASE_URL: str = os.getenv("DATABASE_URL", "sqlite+aiosqlite:///./poc.db")
+
+    # Application Settings
+    TEMPORAL_WORKER_TASK_QUEUE: Dict[str, str] = json.loads(
+        os.getenv("TEMPORAL_WORKER_TASK_QUEUE", "{}")
+    )
+
+
+env = EnvSettings()
